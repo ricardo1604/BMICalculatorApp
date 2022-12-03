9b8788ee456f90a99613c8de6fe0260101b8bd62
rsosa.ramos <rsosa.ramos@gmail.com>
2022-12-02 23:55:04 -0600

    Initial Commit

---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+  edad = 25;
+  peso = 60;
+  altura = 170;
+  sexo = 'Masculino'
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
+  }
+
+  masculino() {
+this.sexo = 'Masculino';
+  }
+
+  femenino() {
+this.sexo = 'Femenino';
+  }
+
+  calcularBMI() {
+  this.router.navigate(['/resultado'])
+  }
+}
